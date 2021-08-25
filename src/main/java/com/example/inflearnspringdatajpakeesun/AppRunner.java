4a190e28ab2b31eae21f06bf59b7bfb7bdaef91b
KwangtaekJung <kt79.jung@gmail.com>
2021-08-25 17:52:51 +0900

    JPA 프로그래밍: 7.쿼리

--- src/main/java/com/example/inflearnspringdatajpakeesun/AppRunner.java
+++ src/main/java/com/example/inflearnspringdatajpakeesun/AppRunner.java
@@ -1,13 +1,14 @@
 package com.example.inflearnspringdatajpakeesun;
 
-import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Component
 @Transactional
@@ -18,28 +19,11 @@ public class AppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-//        Post post = new Post();
-//        post.setTitle("Spring Data JPA...");
-//
-//        Comment comment = new Comment();
-//        comment.setComment("빨리 보고 싶어요.");
-//        post.addComment(comment);
-//
-//        Comment comment1 = new Comment();
-//        comment1.setComment("곧 보여드릴게요.");
-//        post.addComment(comment1);
-//
-//        Session session = entityManager.unwrap(Session.class);
-//        session.save(post);
+        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post AS p", Post.class);
+        List<Post> posts = query.getResultList();
+        posts.forEach(System.out::println);
 
-        Session session = entityManager.unwrap(Session.class);
-        Post post = session.get(Post.class, 4l);
-        System.out.println("===================");
-        System.out.println(post.getTitle());
-
-        post.getComments().forEach( c -> {
-            System.out.println("--------------");
-            System.out.println(c.getComment());
-        });
+        List<Post> posts1 = entityManager.createNativeQuery("select * from Post", Post.class).getResultList();
+        posts1.forEach(System.out::println);
     }
 }
--- src/main/java/com/example/inflearnspringdatajpakeesun/Post.java
+++ src/main/java/com/example/inflearnspringdatajpakeesun/Post.java
@@ -2,6 +2,7 @@ package com.example.inflearnspringdatajpakeesun;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import java.util.Set;
 
 @Entity
 @Getter @Setter
+@ToString(exclude = "comments")
 public class Post {
 
     @Id @GeneratedValue
